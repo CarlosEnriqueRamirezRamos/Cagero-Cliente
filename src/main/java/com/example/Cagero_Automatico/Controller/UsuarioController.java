0431609a2f9d4620605c21675be85c7407b8d449
alien 4 <alien 4@ALIEN4>
2025-06-25 18:36:41 -0600

    Actualizacion del Cliente 25/06/2025 Sin el security

--- src/main/java/com/example/Cagero_Automatico/Controller/UsuarioController.java
+++ src/main/java/com/example/Cagero_Automatico/Controller/UsuarioController.java
@@ -16,6 +16,7 @@ import org.springframework.web.client.RestTemplate;
 
 import java.util.ArrayList;
 import java.util.List;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 @RequestMapping("/Cagero")
